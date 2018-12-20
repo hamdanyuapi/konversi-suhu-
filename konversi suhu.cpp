2f87da888269d914ee843e16c230faef1e45d698
hamdanyuapi <45725263+hamdanyuapi@users.noreply.github.com>
2018-12-20 20:48:39 +0700

    Add files via upload

---
+++ konversi suhu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+    int main(){
+    float C,f,k,r;
+    cout<<"program konversi suhu"<<endl;
+    cout<<"pelitabangsa.com"<<endl<<endl;
+    cout<<"masukan nilai celciusnya = ";
+    cin>>C;
+    f=C*1.8 + 32;
+    k=C+273.15;
+    r=C*0.8;
+    cout<<"nilai farenhit = "<<endl<<"nilai kelvin = "<<endl<<"nilai reamur ="<<r;
+    return 0;
+
+    }
